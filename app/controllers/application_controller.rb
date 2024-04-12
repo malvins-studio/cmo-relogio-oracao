ea2a1cfebf4e537314473540eeeda3a961e658a7
Michael Lins <michael@malvins.studio>
2024-04-12 20:49:24 -0300

    [wip] Including new layout and PNA data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def lazy_load_pnas
+    require 'csv'
+    $pnas ||= CSV.read 'unreached_peoples.csv', headers: true, header_converters: :symbol, converters: [:all]
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @pnas = lazy_load_pnas[0..3]
   end
 end
