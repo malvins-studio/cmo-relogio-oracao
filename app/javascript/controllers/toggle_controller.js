3fb0ea85ea2d7564541681e83fd0395a248b2bdf
Michael Lins <michael@malvins.studio>
2024-04-13 06:45:55 -0300

    fix: import of Stimulus in controller

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,9 +1,9 @@
-import { Controller } from "stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["content"]
   
     toggle() {
-        this.contentTargets.forEach((t) => t.classList.toggle(data.get("class")));
-    }
+    this.contentTargets.forEach((t) => t.classList.toggle(data.get("class")));
+  }
 }
